ab87fac6743cd652bbd74f987b6bcd8dff15f220
utkarshsta44 <147502135+utkarshsta44@users.noreply.github.com>
2023-10-19 17:56:50 +0530

    Add files via upload

---
+++ banking system  database.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE bank;
+USE bank;
+CREATE TABLE customer
+   (
+       custid VARCHAR(6),
+       fname VARCHAR(30),
+       mname VARCHAR(30),
+       ltname VARCHAR(30),
+       city VARCHAR(15),
+       mobileno VARCHAR(10),
+       occupation VARCHAR(10),
+       dob DATE,
+      CONSTRAINT customer_custid_pk PRIMARY KEY(custid)   
+   );
+
+CREATE TABLE branch
+   (
+    bid VARCHAR(6),
+    bname VARCHAR(30),
+    bcity VARCHAR(30),
+    CONSTRAINT branch_bid_pk PRIMARY KEY(bid) 
+   );
+
+   CREATE TABLE account
+   (
+      acnumber VARCHAR(6),
+      custid  VARCHAR(6),
+      bid VARCHAR(6),
+      opening_balance INT,
+      aod DATE,
+      atype VARCHAR(10),
+      astatus VARCHAR(10),
+      CONSTRAINT account_acnumber_pk PRIMARY KEY(acnumber),
+      CONSTRAINT account_custid_fk FOREIGN KEY(custid) REFERENCES customer(custid),
+      CONSTRAINT account_bid_fk FOREIGN KEY(bid) REFERENCES branch(bid) 
+    );
+
+	CREATE TABLE trandetails
+    (   
+     tnumber VARCHAR(6),
+     acnumber VARCHAR(6),
+     dot DATE,
+     medium_of_transaction VARCHAR(20),
+     transaction_type VARCHAR(20),
+     transaction_amount INT,    
+     CONSTRAINT trandetails_tnumber_pk PRIMARY KEY(tnumber),
+     CONSTRAINT trandetails_acnumber_fk FOREIGN KEY(acnumber) REFERENCES account(acnumber)  
+    );
+
+	CREATE TABLE loan
+   (
+   
+    custid VARCHAR(6),
+    bid VARCHAR(6),
+    loan_amount INT,
+    CONSTRAINT loan_customer_custid_bid_pk PRIMARY KEY(custid,bid),
+    CONSTRAINT loan_custid_fk FOREIGN KEY(custid) REFERENCES  customer(custid),
+    CONSTRAINT loan_bid_fk FOREIGN KEY(bid) REFERENCES  branch(bid)
+   );
+
+   INSERT INTO customer VALUES('C00001','Ramesh','Chandra','Sharma','Delhi','9543198345','Service','1976-12-06');
+INSERT INTO customer VALUES('C00002','Avinash','Sunder','Minha','Delhi','9876532109','Service','1974-10-16');
+INSERT INTO customer VALUES('C00003','Rahul',null,'Rastogi','Delhi','9765178901','Student','1981-09-26');
+INSERT INTO customer VALUES('C00004','Parul',null,'Gandhi','Delhi','9876532109','Housewife','1976-11-03');
+INSERT INTO customer VALUES('C00005','Naveen','Chandra','Aedekar','Mumbai','8976523190','Service','1976-09-19');
+INSERT INTO customer VALUES('C00006','Chitresh',null,'Barwe','Mumbai','7651298321','Student','1992-11-06');
+INSERT INTO customer VALUES('C00007','Amit','Kumar','Borkar','Mumbai','9875189761','Student','1981-09-06');
+INSERT INTO customer VALUES('C00008','Nisha',null,'Damle','Mumbai','7954198761','Service','1975-12-03');
+INSERT INTO customer VALUES('C00009','Abhishek',null,'Dutta','Kolkata','9856198761','Service','1973-05-22');
+INSERT INTO customer  VALUES('C00010','Shankar',null,'Nair','Chennai','8765489076','Service','1976-07-12');
+
+
+INSERT INTO branch VALUES('B00001','Asaf ali road','Delhi');
+INSERT INTO branch VALUES('B00002','New delhi main branch','Delhi');
+INSERT INTO branch VALUES('B00003','Delhi cantt','Delhi');
+INSERT INTO branch VALUES('B00004','Jasola','Delhi');
+INSERT INTO branch VALUES('B00005','Mahim','Mumbai');
+INSERT INTO branch VALUES('B00006','Vile parle','Mumbai');
+INSERT INTO branch VALUES('B00007','Mandvi','Mumbai');
+INSERT INTO branch VALUES('B00008','Jadavpur','Kolkata');
+INSERT INTO branch VALUES('B00009','Kodambakkam','Chennai');
